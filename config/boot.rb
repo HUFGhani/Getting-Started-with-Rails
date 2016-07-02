d019dca963de3e088a918f3cd6fa12cfa8dfc2d0
Hamza Ghani <h.u.f.ghani@gmail.com>
2016-07-02 13:54:42 +0000

    ruby on rails server config (to get it work on my network)

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+
+require 'rails/commands/server'
+
+module Rails
+	class Server
+		def default_options
+			super.merge({Host:  '192.168.0.19', Port: 6562})
+		end
+	end
+end
